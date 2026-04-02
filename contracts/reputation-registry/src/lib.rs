0b5cd92481fb6fc2c182a657d2a1b5e3246653f6
0x471 <0x471@protonmail.com>
2026-04-02 03:44:12 +0100

    chore: add contractmeta versioning to all contracts

--- contracts/reputation-registry/src/lib.rs
+++ contracts/reputation-registry/src/lib.rs
@@ -1,5 +1,9 @@
 #![no_std]
 
+use soroban_sdk::contractmeta;
+contractmeta!(key = "Description", val = "ERC-8004 Reputation Registry");
+contractmeta!(key = "Version", val = "0.1.0");
+
 pub mod contract;
 pub mod errors;
 pub mod events;
